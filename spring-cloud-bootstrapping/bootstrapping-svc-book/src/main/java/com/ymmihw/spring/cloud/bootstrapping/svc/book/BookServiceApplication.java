aa4a540ba5b61726ff85f13404b2f50de9145b4b
whimmy <whimmy@126.com>
2018-04-15 14:19:13 +0800

    clean up

--- spring-cloud-bootstrapping/bootstrapping-svc-book/src/main/java/com/ymmihw/spring/cloud/bootstrapping/svc/book/BookServiceApplication.java
+++ spring-cloud-bootstrapping/bootstrapping-svc-book/src/main/java/com/ymmihw/spring/cloud/bootstrapping/svc/book/BookServiceApplication.java
@@ -15,22 +15,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/books")
 public class BookServiceApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(BookServiceApplication.class, args);
-    }
- 
-    private List<Book> bookList = Arrays.asList(
-        new Book(1L, "Baeldung goes to the market", "Tim Schimandle"),
-        new Book(2L, "Baeldung goes to the park", "Slavisa")
-    );
- 
-    @GetMapping("")
-    public List<Book> findAllBooks() {
-        return bookList;
-    }
- 
-    @GetMapping("/{bookId}")
-    public Book findBook(@PathVariable Long bookId) {
-        return bookList.stream().filter(b -> b.getId().equals(bookId)).findFirst().orElse(null);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(BookServiceApplication.class, args);
+  }
+
+  private List<Book> bookList =
+      Arrays.asList(new Book(1L, "Baeldung goes to the market", "Tim Schimandle"),
+          new Book(2L, "Baeldung goes to the park", "Slavisa"));
+
+  @GetMapping("")
+  public List<Book> findAllBooks() {
+    return bookList;
+  }
+
+  @GetMapping("/{bookId}")
+  public Book findBook(@PathVariable Long bookId) {
+    return bookList.stream().filter(b -> b.getId().equals(bookId)).findFirst().orElse(null);
+  }
 }
