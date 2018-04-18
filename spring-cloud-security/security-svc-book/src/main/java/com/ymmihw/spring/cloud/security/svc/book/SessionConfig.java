91f3d726d4517510b7fd353df8998f393abb4f97
whimmy <whimmy@126.com>
2018-04-19 06:47:58 +0800

    clean up

--- spring-cloud-security/security-svc-book/src/main/java/com/ymmihw/spring/cloud/security/svc/book/SessionConfig.java
+++ spring-cloud-security/security-svc-book/src/main/java/com/ymmihw/spring/cloud/security/svc/book/SessionConfig.java
@@ -1,12 +1,10 @@
 package com.ymmihw.spring.cloud.security.svc.book;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.session.web.context.AbstractHttpSessionApplicationInitializer;
 import org.springframework.session.web.http.DefaultCookieSerializer;
 
-@Configuration
 @EnableRedisHttpSession
 public class SessionConfig extends AbstractHttpSessionApplicationInitializer {
   @Bean
