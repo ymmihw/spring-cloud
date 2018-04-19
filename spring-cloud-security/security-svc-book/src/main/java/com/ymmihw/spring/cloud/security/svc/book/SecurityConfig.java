6058086aad4abc1fc17377903bb883961008284d
wanghaoming <whimmy@126.com>
2018-04-19 13:39:30 +0800

    clean up

--- spring-cloud-security/security-svc-book/src/main/java/com/ymmihw/spring/cloud/security/svc/book/SecurityConfig.java
+++ spring-cloud-security/security-svc-book/src/main/java/com/ymmihw/spring/cloud/security/svc/book/SecurityConfig.java
@@ -1,14 +1,12 @@
 package com.ymmihw.spring.cloud.security.svc.book;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
-@Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Autowired
@@ -18,8 +16,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-    http.httpBasic().disable().authorizeRequests().antMatchers("/books").permitAll()
-        .antMatchers("/books/*").hasAnyRole("USER", "ADMIN").anyRequest().authenticated().and()
+    // @formatter:off
+    http.httpBasic().disable()
+        .authorizeRequests()
+            .antMatchers("/books").permitAll()
+            .antMatchers("/books/*").hasAnyRole("USER", "ADMIN")
+            .anyRequest().authenticated().and()
         .csrf().disable();
+    // @formatter:on
   }
 }
