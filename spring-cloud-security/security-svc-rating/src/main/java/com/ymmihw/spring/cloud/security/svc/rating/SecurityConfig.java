bb9109d8540e10ee5c0d1c8a6d95c15d1067d969
wanghaoming <whimmy@126.com>
2018-04-19 13:43:10 +0800

    clean up

--- spring-cloud-security/security-svc-rating/src/main/java/com/ymmihw/spring/cloud/security/svc/rating/SecurityConfig.java
+++ spring-cloud-security/security-svc-rating/src/main/java/com/ymmihw/spring/cloud/security/svc/rating/SecurityConfig.java
@@ -1,14 +1,12 @@
 package com.ymmihw.spring.cloud.security.svc.rating;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
-@Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Autowired
@@ -18,8 +16,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-    http.httpBasic().disable().authorizeRequests().antMatchers("/ratings/*").hasRole("ADMIN")
-        .antMatchers("/ratings").hasAnyRole("USER", "ADMIN").anyRequest().authenticated().and()
+    // @formatter:off
+    http.httpBasic().disable()
+        .authorizeRequests()
+            .antMatchers("/ratings/*").hasRole("ADMIN")
+            .antMatchers("/ratings").hasAnyRole("USER", "ADMIN")
+            .anyRequest().authenticated().and()
         .csrf().disable();
+    // @formatter:on
   }
 }
