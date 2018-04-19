f2d81750354eac0fa111a17b11d124b7f2dcb1b3
whimmy <whimmy@126.com>
2018-04-20 06:53:59 +0800

    remove legacy

--- spring-cloud-zipkin/zipkin-gateway/src/main/java/com/ymmihw/spring/cloud/zipkin/gateway/GatewayApplication.java
+++ spring-cloud-zipkin/zipkin-gateway/src/main/java/com/ymmihw/spring/cloud/zipkin/gateway/GatewayApplication.java
@@ -1,19 +1,9 @@
 package com.ymmihw.spring.cloud.zipkin.gateway;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.cloud.sleuth.metric.SpanMetricReporter;
-import org.springframework.cloud.sleuth.zipkin.HttpZipkinSpanReporter;
-import org.springframework.cloud.sleuth.zipkin.ZipkinSpanReporter;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
-import com.netflix.appinfo.InstanceInfo;
-import com.netflix.discovery.EurekaClient;
-import zipkin.Span;
 
 @SpringBootApplication
 @EnableZuulProxy
@@ -21,33 +11,5 @@ import zipkin.Span;
 public class GatewayApplication {
   public static void main(String[] args) {
     SpringApplication.run(GatewayApplication.class, args);
-  }
-
-  @Autowired
-  private EurekaClient eurekaClient;
-
-  @Autowired
-  private SpanMetricReporter spanMetricReporter;
-
-  @Value("${spring.sleuth.web.skipPattern}")
-  private String skipPattern;
-
-  @Bean
-  public ZipkinSpanReporter makeZipkinSpanReporter() {
-    return new ZipkinSpanReporter() {
-      private HttpZipkinSpanReporter delegate;
-      private String baseUrl;
-
-      @Override
-      public void report(Span span) {
-        InstanceInfo instance = eurekaClient.getNextServerFromEureka("zipkin", false);
-        if (!(baseUrl != null && instance.getHomePageUrl().equals(baseUrl))) {
-          baseUrl = instance.getHomePageUrl();
-          delegate = new HttpZipkinSpanReporter(new RestTemplate(), baseUrl, 1, spanMetricReporter);
-        }
-        if (!span.name.matches(skipPattern))
-          delegate.report(span);
-      }
-    };
   }
 }
