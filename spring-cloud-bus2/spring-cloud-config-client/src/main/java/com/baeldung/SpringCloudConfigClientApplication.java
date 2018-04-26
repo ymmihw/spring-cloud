f86c6f947a0d83db50283593428afba65ded6664
wanghaoming <whimmy@126.com>
2018-04-26 11:42:00 +0800

    clean up

--- spring-cloud-bus2/spring-cloud-config-client/src/main/java/com/baeldung/SpringCloudConfigClientApplication.java
+++ spring-cloud-bus2/spring-cloud-config-client/src/main/java/com/baeldung/SpringCloudConfigClientApplication.java
@@ -15,18 +15,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RefreshScope
 public class SpringCloudConfigClientApplication {
 
-    @Value("${user.role}")
-    private String role;
+  @Value("${user.role}")
+  private String role;
 
-    @Value("${user.password}")
-    private String password;
+  @Value("${user.password}")
+  private String password;
 
-    public static void main(String[] args) {
-        SpringApplication.run(SpringCloudConfigClientApplication.class, args);
-    }
-    
-    @RequestMapping(value = "/whoami/{username}", method = RequestMethod.GET, produces = MediaType.TEXT_PLAIN_VALUE)
-    public String whoami(@PathVariable("username") String username) {
-        return String.format("Hello %s! You are a(n) %s and your password is '%s'.\n", username, role, password);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(SpringCloudConfigClientApplication.class, args);
+  }
+
+  @RequestMapping(value = "/whoami/{username}", method = RequestMethod.GET,
+      produces = MediaType.TEXT_PLAIN_VALUE)
+  public String whoami(@PathVariable("username") String username) {
+    return String.format("Hello %s! You are a(n) %s and your password is '%s'.\n", username, role,
+        password);
+  }
 }
